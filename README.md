d7c1b0484c81c0fd409418d9f3a5fdc669347744
@hexlet/cli <support@hexlet.io>
2022-11-16 21:00:37 +0300

    submit java-oop-ru/errors

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # Hexlet assignments
-
-[![hexlet-check](https://github.com/agsamkin/hexlet-assignments/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/agsamkin/hexlet-assignments/actions/workflows/hexlet-check.yml)
---
+++ java-oop-ru/errors/src/main/java/exercise/Circle.java
@@ -0,0 +1,25 @@
+package exercise;
+
+// BEGIN
+class Circle {
+    private final Point point;
+    private final int radius;
+
+    public Circle(Point point, int radius) {
+        this.point = point;
+        this.radius = radius;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public int getSquare() throws NegativeRadiusException {
+        if (radius < 0) {
+            throw new NegativeRadiusException("Не удалось посчитать площадь");
+        }
+        double square = Math.PI * Math.pow(radius, 2);
+        return (int) Math.round(square);
+    }
+}
+// END
